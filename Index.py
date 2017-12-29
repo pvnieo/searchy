6ea8e5c05718b52fc0f43eb06630c1d594645113
ayoub <ayoub.sbai@student.ecp.fr>
2017-12-29 15:36:02 +0000

    Created united tool

--- Index.py
+++ Index.py
@@ -1,34 +1,35 @@
-# stdlib
-import pickle
-import os.path
 # project
 from Document import Document
-from utils import insert_sorted, hash_file
+from utils import insert_sorted, hash_collection, get_cache, set_cache
 
 
 class Index:
 
     @staticmethod
-    def index_all(docs):
+    def index_all(docs, cache_name=None, use_cache=True, overwrite_cache=False):        
+        if use_cache and not overwrite_cache:
+            cached = get_cache(cache_name)
+            if cached is not None:
+                return cached
         index = Index()
         for doc in docs:
             index.add_doc(doc)
+        if use_cache:
+            set_cache(cache_name, index)
         return index
 
     @staticmethod
-    def index_cacm_file(filepath, verbose=True, use_cache=True):
-        cachepath = os.path.join('__cache__', hash_file(filepath) + '.index.bin')
-        if use_cache and os.path.exists(cachepath):
-            with open(cachepath, 'rb') as cache:
-                try:
-                    return pickle.load(cache)
-                except pickle.UnpicklingError:
-                    pass
-        docs = Document.parse_cacm(filepath, verbose, use_cache)
-        index = Index.index_all(docs)
-        if use_cache:
-            with open(cachepath, 'wb') as cache:
-                pickle.dump(index, cache)
+    def index_cacm_file(filepath, verbose=True, use_cache=True, overwrite_cache=False):
+        cache_name = hash_collection(filepath) + '.index.bin'
+        docs = Document.parse_cacm(filepath, verbose, use_cache, overwrite_cache)
+        index = Index.index_all(docs, cache_name, use_cache, overwrite_cache)
+        return index
+
+    @staticmethod
+    def index_directory(dirpath, verbose=True, use_cache=True, overwrite_cache=False, hold_content=False, buffer_size=5000):
+        cache_name = hash_collection(dirpath) + '.index.bin'
+        docs = Document.read_dir(dirpath, verbose, use_cache, overwrite_cache, hold_content)
+        index = Index.index_all(docs, cache_name, use_cache, overwrite_cache)
         return index
 
     def __init__(self):
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import re
 import hashlib
-import os.path, os
+import os.path
+import os
+import sys
+import pickle
+from zipfile import ZipFile
+from urllib.request import urlretrieve
 
 
 # To print colors in terminal
@@ -28,18 +33,68 @@ def insert_sorted(array, element):
             return
     array.append(element)
 
-def hash_file(filepath):
-    BUF_SIZE = 65536  # 64kb chunks
-    sha1 = hashlib.sha1()
+def hash_collection(path):
+    hashed = hashlib.sha1((path + str(os.path.getsize(path))+str(os.path.getmtime(path))).encode())
+    return hashed.hexdigest()
 
-    with open(filepath, 'rb') as f:
-        while True:
-            data = f.read(BUF_SIZE)
-            if not data:
-                break
-            sha1.update(data)
+CACHE_DIR = '__cache__'
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
 
-    return sha1.hexdigest()
+def get_cache(cachefile):
+    cachefile = os.path.join(CACHE_DIR, cachefile)
+    if os.path.exists(cachefile):
+        with open(cachefile, 'rb') as cache:
+            try:
+                return pickle.load(cache)
+            except pickle.UnpicklingError:
+                pass
+    return None
 
-if not os.path.exists('__cache__'):
-    os.makedirs('__cache__')
+def set_cache(cachefile, obj):
+    cachefile = os.path.join(CACHE_DIR, cachefile)
+    with open(cachefile, 'wb') as cache:
+        pickle.dump(obj, cache)
+
+def download_collection(url):
+    path = os.path.join(CACHE_DIR, os.path.basename(url))
+    download(url, path)
+    splitext = os.path.splitext(os.path.basename(path))
+    if splitext[1] == '.zip':
+        dest = os.path.join(CACHE_DIR, splitext[0])
+        extract_zip(path, dest)
+        return dest
+    return path
+
+def download(url, path):
+    if not os.path.exists(path):
+        sys.stderr.write("downloading %s\n" % url)
+        urlretrieve(url, path, reporthook)
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        sys.stderr.write("\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize))
+        if readsofar >= totalsize:
+            sys.stderr.write("\n")
+    else:
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+def extract_zip(filepath, destpath):
+    if os.path.exists(destpath):
+        return
+    sys.stderr.write("extracting %s\n" % (os.path.basename(filepath)))
+    with ZipFile(filepath) as openedzip:
+        openedzip.extractall(path=destpath)
+
+def is_url(string):
+    pattern = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return pattern.match(string) is not None
